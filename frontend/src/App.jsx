197863492a3ae9a9723b173da7efb31b1243e9e6
You <=>
2025-07-02 19:00:22 +0530

    added structure files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
-import LoginPage from '../pages/Login/LoginPage'
-import { Route, Routes, Link, useLocation } from "react-router-dom"
+import "./App.css";
+import LoginPage from "./pages/Login/LoginPage";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/ReactSelect/ReactSelect.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function ReactSelect(values, setValues) {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Age</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={age}
+        label="Age"
+        onChange={handleChange}
+      >
+        <MenuItem value={10}>Student</MenuItem>
+        <MenuItem value={20}>Faculty</MenuItem>
+        <MenuItem value={30}>Admin</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
